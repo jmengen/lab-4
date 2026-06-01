60e1cd8ae1a8c0352a090d96423889503b1a14ea
Maximus <juanmengen0@gmail.com>
2026-06-01 11:39:00 -0300

    controlALtaUsuarios actualizados

--- include/IControlAltaUsuario.h
+++ include/IControlAltaUsuario.h
@@ -1,5 +1,5 @@
-#ifndef IALTAUSUARIO_H
-#define IALTAUSUARIO_H
+#ifndef ICONTROLALTAUSUARIO_H
+#define ICONTROLALTAUSUARIO_H
 #include <iostream>
 #include <string>
 #include "TipoLibreta.h"
@@ -7,15 +7,12 @@
 #include <set>
 using namespace std;
 
-
-
-
-class IAltaUsuario{
+class IControlAltaUsuario{
     public:
         virtual bool altaPasajero(std::string nickname, std::string nombre, std::string contrasena, std::string email, std::string ci) = 0;
         virtual bool altaConductor(string nickname, string nombre, string contrasena, string email, set<TipoLibreta> libs) = 0;
         virtual int registrarVehiculo(string nickname, string matricula, int capacidad, string marca, string modelo, TipoVehiculo tipo) = 0;
-        virtual ~IAltaUsuario() = default;
+        virtual ~IControlAltaUsuario() = default;
 };
 
 
